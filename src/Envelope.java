77040bfec51e47c9be744e36ab5d2f97c9cae100
Eric <51395805+eric781ctf@users.noreply.github.com>
2020-10-25 20:23:34 +0800

    Add files via upload

---
+++ src/Envelope.java
@@ -0,0 +1,87 @@
+//import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/* $Id: Envelope.java,v 1.8 1999/09/06 16:43:20 kangasha Exp $ */
+
+/**
+ * SMTP envelope for one mail message.
+ * 
+ * @author Jussi Kangasharju
+ */
+public class Envelope {
+	/* SMTP-sender of the message (in this case, contents of From-header. */
+	public String Sender;
+
+	/* SMTP-recipient, or contents of To-header. */
+	public String Recipient;
+
+	/* Target MX-host */
+	public String DestHost;
+	public InetAddress DestAddr;
+
+	/* The actual message */
+	public Message Message;
+
+	/* Create the envelope. */
+	public Envelope(Message message, String localServer)
+			throws UnknownHostException {
+		/* Get sender and recipient. */
+		Sender = message.getFrom();
+		Recipient = message.getTo();
+
+		/*
+		 * Get message. We must escape the message to make sure that there are
+		 * no single periods on a line. This would mess up sending the mail.
+		 */
+		Message = escapeMessage(message);
+
+		/*
+		 * Take the name of the local mailserver and map it into an InetAddress
+		 */
+		DestHost = localServer;
+		try {
+			DestAddr = InetAddress.getByName(DestHost);
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host: " + DestHost);
+			System.out.println(e);
+			throw e;
+		}
+		return;
+	}
+
+	/*
+	 * Escape the message by doubling all periods at the beginning of a line.
+	 */
+	private Message escapeMessage(Message message) {
+		String escapedBody = "";
+		String token;
+		StringTokenizer parser = new StringTokenizer(message.Body, "\n", true);
+
+		while (parser.hasMoreTokens()) {
+			token = parser.nextToken();
+			if (token.startsWith(".")) {
+				token = "." + token;
+			}
+			escapedBody += token;
+		}
+		message.Body = escapedBody;
+		return message;
+	}
+
+	/* For printing the envelope. Only for debug. */
+	public String toString() {
+		String res = "Sender: " + Sender + '\n';
+		System.out.println("1");
+		res += "Recipient: " + Recipient + '\n';
+		System.out.println("2");
+		res += "MX-host: " + DestHost + ", address: " + DestAddr + '\n';
+		System.out.println("3");
+		res += "Message:" + '\n';
+		System.out.println("4");
+		res += Message.toString();
+		System.out.println("Return res");
+
+		return res;
+	}
+}
